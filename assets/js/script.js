73a5e72f549462f1a4dccc3da71d772b060ee962
Nandipati Ayyappa Reddy <119839645+ayyappareddynandipati@users.noreply.github.com>
2025-04-06 10:44:40 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var typed = new Typed(".text", {
-      strings: ["Frontend Developer", "Problem Solver"],
-      typeSpeed: 100,
-      backSpeed: 100,
-      backDelay: 1000,
-      loop: true,
-    });
-  });
-
- 
  // Function to set the theme and update UI
  function setTheme(theme) {
     document.body.setAttribute('data-bs-theme', theme);
